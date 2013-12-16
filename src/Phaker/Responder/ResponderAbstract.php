b1f9487688ed2515b1e0c50a0a7ef3354a48ae07
Dejan Samardzija <samardzija.dejan+hq@gmail.com>
2013-12-16 01:04:47 +0100

    added index method, cleanup for responder classes

--- src/Phaker/Responder/ResponderAbstract.php
+++ src/Phaker/Responder/ResponderAbstract.php
@@ -140,6 +140,29 @@ abstract class ResponderAbstract
      *
      * @return Phaker\Response\ResponseAbstract
      */
-    abstract public function respond();
+    public function respond()
+    {
+        $serviceClass = $this->getServiceClass();
+        $service = new $serviceClass($this->getServiceArguments());
+
+        $responseClass = $this->getResponseClass();
+        $response = new $responseClass;
+
+        foreach($response->getAllHeaders() as $key => $value) {
+            header(sprintf("%s: %s", $key, $value));
+        }
+
+        header(sprintf("HTTP/1.1 %d %s", $response->getHttpCode(), $response->getHttpMessage()), true, $response->getHttpCode());
+
+        $output = array(
+            'code'     => $response->getHttpCode(),
+            'message'  => $response->getHttpMessage(),
+            'response' => $service->getData(),
+            'request'  => '',
+            'version'  => '2.1.5',
+        );
+
+        echo json_encode($output, JSON_PRETTY_PRINT);
+    }
 
 }
